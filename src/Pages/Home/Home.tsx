2d0d554716ea87558fee43bb7fe781bb745c872b
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-05-28 23:32:01 +0300

    14

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ImagesSlider from '../../Components/ImagesSlider/ImagesSlider';
-import image1 from '../../Components/Images/web_papa_jon_tree_pizza_52-grn_1514_300.jpg';
+import image1 from '../../Components/Images/web_papa_jon_tree_pizza_53-grn_1514_300.jpg';
 import image2 from '../../Components/Images/week_offer_delivery_page_1414_300_papa-dzhon.jpg';
 import image3 from '../../Components/Images/deliveryB.jpg';
 import image4 from '../../Components/Images/web_-50_na_drugu_1314_300B.jpg'
