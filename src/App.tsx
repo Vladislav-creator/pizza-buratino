a9c0f9067799b04a76f195c5dfebee845c241348
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-05-28 17:37:02 +0300

    4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from './Pages/NotFound/NotFound';
 import Navigation from './Components/Navigation/Navigation';
 import { Loader } from './Components/Loader';
