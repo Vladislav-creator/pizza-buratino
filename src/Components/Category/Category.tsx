35b72d72437181351e99b53a24d1ab0fd465fe99
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-06-05 00:46:58 +0300

    234

--- src/Components/Category/Category.tsx
+++ src/Components/Category/Category.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import PizzaItem from '../PizzaItem/PizzaItem';
 import { Pizza } from '../Types/Pizza';
-
+import css from './Category.module.css'
 interface CategoryProps {
   category: string;
   pizzas: Pizza[];
+  
 }
 
 const Category: React.FC<CategoryProps> = ({ category, pizzas }) => {
   return (
     <div>
-      <h1>{category}</h1>
+      <h2 className={css.subTitle}>{category}</h2>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {pizzas.map((pizza) => (
           <PizzaItem key={pizza.id} pizza={pizza} />
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,4 @@
-
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { getAllcarPages } from '../../services';
 import Category from '../../Components/Category/Category';
 import { Pizza } from '../../Components/Types/Pizza';
@@ -9,28 +8,38 @@ import image2 from '../../Components/Images/week_offer_delivery_page_1414_300_pa
 import image3 from '../../Components/Images/deliveryC.jpg';
 import image4 from '../../Components/Images/web_-50_na_drugu_1314_300B.jpg';
 import AllIcon from '../../Components/Images/all';
-// import Image from '../../Components/Image';
 import LeafIcon from '../../Components/Images/vegan';
 import NewIcon from '../../Components/Images/new';
 import FireIcon from '../../Components/Images/fire';
 import HeardIcon from '../../Components/Images/heard';
 import css from './Home.module.css';
+
 const Home: React.FC = () => {
   const images = [image1, image2, image3, image4];
-  const [pizzas, setPizzas] = useState<Pizza[]>([]);
+  const [allPizzas, setAllPizzas] = useState<Pizza[]>([]);
+  const [displayedPizzas, setDisplayedPizzas] = useState<Pizza[]>([]);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
-  const observer = useRef<IntersectionObserver | null>(null);
+  const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
+
+  const toggleFilter = (filter: string) => {
+    setSelectedFilters(prevFilters => {
+      if (prevFilters.includes(filter)) {
+        return prevFilters.filter(f => f !== filter);
+      } else {
+        return [...prevFilters, filter];
+      }
+    });
+  };
 
   const fetchPizzas = useCallback(async () => {
     setLoading(true);
     try {
-      const newPizzas = await getAllcarPages(pizzas.length / 9 + 1);
-      console.log('Fetched pizzas:', newPizzas);
+      const newPizzas = await getAllcarPages(Math.ceil(allPizzas.length / 9 + 1));
       if (newPizzas.length === 0) {
         setHasMore(false);
       } else {
-        setPizzas((prevPizzas) => {
+        setAllPizzas((prevPizzas) => {
           const combinedPizzas = [...prevPizzas, ...newPizzas];
           const uniquePizzas = combinedPizzas.filter(
             (pizza, index, self) => index === self.findIndex((p) => p.id === pizza.id)
@@ -43,73 +52,126 @@ const Home: React.FC = () => {
     } finally {
       setLoading(false);
     }
-  }, [pizzas.length]);
+  }, [allPizzas.length]);
 
   useEffect(() => {
     fetchPizzas();
   }, [fetchPizzas]);
 
   useEffect(() => {
-    if (observer.current) observer.current.disconnect();
-
-    const callback = (entries: IntersectionObserverEntry[]) => {
+    const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
       if (entries[0].isIntersecting && !loading && hasMore) {
         fetchPizzas();
       }
-    };
+    });
 
-    observer.current = new IntersectionObserver(callback);
-    observer.current.observe(document.querySelector('#loadMore')!);
+    observer.observe(document.querySelector('#loadMore')!);
 
-    return () => observer.current?.disconnect();
+    return () => observer.disconnect();
   }, [loading, hasMore, fetchPizzas]);
 
-  const groupedPizzas = pizzas.reduce((acc: { [key: number]: Pizza[] }, pizza) => {
-    if (!acc[pizza.level]) {
-      acc[pizza.level] = [];
+  const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCategory = e.target.value;
+    if (selectedCategory) {
+      const element = document.getElementById(selectedCategory);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
     }
-    acc[pizza.level].push(pizza);
-    return acc;
-  }, {});
+  };
+
+  const applyFilters = useCallback(() => {
+    const filteredPizzas = allPizzas.filter(pizza => {
+      if (selectedFilters.length === 0) {
+        return true;
+      }
+      return selectedFilters.every(filter => {
+        switch (filter) {
+          case 'vegetarian':
+            return pizza.vegetarian === true;
+          case 'new':
+            return pizza.new === true;
+          case 'spicy':
+            return pizza.spicy === true;
+          case 'popular':
+            return pizza.popular === true;
+          default:
+            return true;
+        }
+      });
+    });
+
+    console.log('Filtered Pizzas:', filteredPizzas);
+    setDisplayedPizzas(filteredPizzas);
+  }, [allPizzas, selectedFilters]);
+
+  useEffect(() => {
+    applyFilters();
+  }, [applyFilters, selectedFilters]);
 
   return (
     <div className="container">
       <h1>PIZZA BURATINO</h1>
       <div className={css.iconsAndSlider}>
         <div>
-      <div className={css.icons}>
-   <LeafIcon width ='50px'/>
-   <NewIcon width ='50px'/>
-   <FireIcon width ='50px'/>
-   <HeardIcon width ='50px'/>
-   <AllIcon width ='50px'/>
-   </div>
-   <select name="" id=""></select>
-   </div>
-      <ImagesSlider images={images} interval={3000} />
-      {/* <Image
-        src="https://buratino-images.s3.eu-north-1.amazonaws.com/Pizza-Buratino/%D1%81ombo_sets/combo_sets_%E2%84%961.png"
-        alt="A sample image from Google Drive"
-      /> */}
-      
-   </div>
-   <div>
-   {Object.keys(groupedPizzas)
-        .sort((a, b) => parseInt(a) - parseInt(b)) // Сортировка уровней по возрастанию
-        .map((level) => (
-          <Category
-            key={level}
-            category={groupedPizzas[parseInt(level)][0].categories}
-            pizzas={groupedPizzas[parseInt(level)]}
-          />
-        ))}
-      <div id="loadMore" style={{ height: '1px' }}></div>
-      {loading && <p>Loading more pizzas...</p>}
-      {!hasMore && <p>No more pizzas to load.</p>}
-    </div>
-    </div>
-   
+          <div className={css.icons}>
+            <div onClick={() => {
+              setSelectedFilters([]);
+              toggleFilter('vegetarian');
+            }}>
+              <LeafIcon width='50px' />
+            </div>
+            <div onClick={() => {
+              setSelectedFilters([]);
+              toggleFilter('new');
+            }}>
+              <NewIcon width='50px' />
+            </div>
+            <div onClick={() => {
+              setSelectedFilters([]);
+              toggleFilter('spicy');
+            }}>
+              <FireIcon width='50px' />
+            </div>
+            <div onClick={() => {
+              setSelectedFilters([]);
+              toggleFilter('popular');
+            }}>
+              <HeardIcon width='50px' />
+            </div>
+            <div onClick={() => setSelectedFilters([])}>
+              <AllIcon width='50px' />
+            </div>
+          </div>
+          <select onChange={handleCategoryChange}>
+            <option value="">All</option>
+            {Array.from(new Set(displayedPizzas.map(pizza => pizza.categories)))
+              .map((category, index) => (
+                <option key={index} value={`category-${index}`}>
+                  {category}
+                </option>
+              ))}
+          </select>
+        </div>
+        <ImagesSlider images={images} interval={3000} />
+      </div>
 
+      <div>
+        {Array.from(new Set(displayedPizzas.map(pizza => pizza.level)))
+          .sort((a, b) => a - b)
+          .map((level, index) => (
+            <div key={level} id={`category-${index}`}>
+              <Category
+                category={displayedPizzas.find(pizza => pizza.level === level)?.categories ?? ''}
+                pizzas={displayedPizzas.filter(pizza => pizza.level === level)}
+              />
+            </div>
+          ))}
+        <div id="loadMore" style={{ height: '1px' }}></div>
+        {loading && <p>Loading more pizzas...</p>}
+        {!hasMore && <p>No more pizzas to load.</p>}
+      </div>
+    </div>
   );
 };
 
