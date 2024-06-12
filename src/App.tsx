c6a6a17d2cc0a70ec787a975ee33792b0115c69f
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-06-12 12:02:08 +0300

    333

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from './Components/ModalDeliveryConditions/ModalDeliveryConditions';
 import { HashRouter, Routes, Route } from 'react-router-dom';
  import NotFound from './Pages/NotFound/NotFound';
 import Navigation from './Components/Navigation/Navigation';
 import { Loader } from './Components/Loader';
 import { Suspense, lazy } from 'react';
 import mySVG from './Components/Images/Buratino_Layer 2.png';
+import ClockIcon from './Components/Images/clock';
 import css from './App.module.css';
 const Home = lazy(() => import('./Pages/Home/Home'));
 const About = lazy(() => import('./Pages/About/About'));
 const Contacts = lazy(() => import('./Pages/Contacts/Contacts'));
 
 const App: React.FC = () => {
+    const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
     return (
         
          <HashRouter>
-            <div className='backgraund-pizza'>
+            <div className={css.backgraundPizza}>
             <Navigation />
-            <img className={css.logo}  src={mySVG} alt="Description" />
+            <div className={css.wrapTimeDelivery}>
+                <div className={css.wrapClockTimeOfWork}><div className={css.divClock}><ClockIcon/></div> <p className={css.timeOfWork}>Доставка працює з 10:00 до 22:00</p></div>
            
+            <img className={css.logo}  src={mySVG} alt="Description" />
+            <div className={css.app}>
+      <button className={css.deliveryConditions} onClick={openModal}>
+        Умови доставки
+      </button>
+      <Modal isOpen={isModalOpen} onClose={closeModal} />
+    </div>
+           </div>
             </div>
             
          <Suspense fallback={<Loader />}>
---
+++ src/Components/ModalDeliveryConditions/ModalDeliveryConditions.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import css from './ModalDeliveryConditions.module.css';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleEsc);
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, [onClose]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className={css.modalBackdrop} onClick={onClose}>
+      <div className={css.modalContent} onClick={(e) => e.stopPropagation()}>
+        <button className={css.closeButton} onClick={onClose}></button>
+        <div className={css.titleModalContent}><h2>Умови доставки Піца Буратіно</h2></div>
+        <div className={css.textModalContent}>
+        <p>Область доставки – Весь Київ</p>
+        <p>Вартість доставки – безкоштовно від 600 грн, від 200 до 599 грн доставка 40 грн.</p>
+        <p>Час роботи:</p>
+        <p>ПН-НД – з 10:00 до 22:00</p>
+        <p>Мінімальна сума замовлення – 200 грн</p>
+        <hr />
+        <p>Акції та знижки між собою не сумуються!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
