0568b1f553ff032cfa92644173844a1c2ec1ab17
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-06-19 10:47:34 +0300

    Updates

---
+++ static/js/498.58e29256.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpizza_buratino=self.webpackChunkpizza_buratino||[]).push([[498],{498:(n,a,e)=>{e.r(a),e.d(a,{default:()=>s});e(43);var i=e(579);const s=()=>(0,i.jsxs)("div",{children:[(0,i.jsx)("h1",{children:"Contact Page"}),(0,i.jsx)("p",{children:"Contact us for any inquiries! And we will answer you"})]})}}]);
+//# sourceMappingURL=498.58e29256.chunk.js.map
