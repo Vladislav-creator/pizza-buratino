878abe37fbe45efedf812a09c8f4ead6f811b7ad
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-07-02 10:32:12 +0300

    111

--- src/Components/Redux/store.tsx
+++ src/Components/Redux/store.tsx
@@ -1,12 +1,38 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import cartReducer from './cartSlice';
+
+// const store = configureStore({
+//   reducer: {
+//     cart: cartReducer,
+//   },
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export default store;
+
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session'; // sessionStorage
 import cartReducer from './cartSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage: storageSession,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartReducer);
 
 const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
-// Явно экспортируем RootState тип
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export default store;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,41 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+// import { Provider } from 'react-redux';
+// import store from './Components/Redux/store';
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//      <Provider store={store}>
+//     <App />
+//   </Provider>
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-
 import { Provider } from 'react-redux';
-import store from './Components/Redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './Components/Redux/store';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-     <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
